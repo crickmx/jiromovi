e8fc05d676ff998d0d9be0c69301970211fe977f
Christofer CCJ <cdcjimenez@gmail.com>
2025-12-18 12:08:59 -0600

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -258,12 +258,11 @@ export function Dashboard() {
           <ProximasCapacitaciones />
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
           <ResumenVacaciones />
           <TramitesWidget />
+          <ProximasReservas />
         </div>
-
-        <ProximasReservas />
       </div>
     );
   }
