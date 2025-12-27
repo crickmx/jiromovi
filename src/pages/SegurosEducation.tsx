58dd95a9d62936db4e68df7563f3bffe04764159
Christofer CCJ <cdcjimenez@gmail.com>
2025-12-27 10:40:19 -0600

    Updated SegurosEducation.tsx

--- src/pages/SegurosEducation.tsx
+++ src/pages/SegurosEducation.tsx
@@ -193,7 +193,7 @@ export function SegurosEducation() {
         <div className="bg-gradient-to-r from-primary-600 to-primary-700 rounded-2xl p-8 text-white">
           <div className="flex items-center justify-center mb-4">
             <img
-              src="/movirecurso_10.svg"
+              src="/movirecurso_11.png"
               alt="Seguros Education"
               className="h-16 sm:h-20 object-contain"
             />
