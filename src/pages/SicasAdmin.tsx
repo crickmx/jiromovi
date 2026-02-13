cb99585d639f29866c6996a75f1d6f84568da423
Christofer CCJ <cdcjimenez@gmail.com>
2026-02-13 11:08:30 -0600

    Updated SicasAdmin.tsx

--- src/pages/SicasAdmin.tsx
+++ src/pages/SicasAdmin.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Loader2, RefreshCw, CheckCircle, XCircle, Building, Users, Trash2, Link as LinkIcon, FlaskConical, Stethoscope } from 'lucide-react';
+import { Loader2, RefreshCw, CheckCircle, XCircle, Building, Users, Trash2, Link as LinkIcon, FlaskConical, Stethoscope, AlertCircle } from 'lucide-react';
 import { Button } from '../components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../components/ui/tabs';
