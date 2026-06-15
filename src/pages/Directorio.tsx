ee9e286de1620981ad9d882a771492ee60f5587f
Christofer CCJ <cdcjimenez@gmail.com>
2026-06-15 11:29:38 -0700

    Updated Directorio.tsx

--- src/pages/Directorio.tsx
+++ src/pages/Directorio.tsx
@@ -79,7 +79,7 @@ export function Directorio() {
       let usuariosQuery = supabase
         .from('usuarios')
         .select('id, nombre, apellidos, email_laboral, email_personal, celular_personal, celular_laboral, username, rol, estado, activo, oficina_id, puesto, imagen_perfil_url, is_deleted, oficinas(nombre)')
-        .eq('is_deleted', false)
+        .or('is_deleted.eq.false,is_deleted.is.null')
         .order('nombre')
         .limit(2000);
 
